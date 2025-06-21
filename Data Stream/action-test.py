fa3f463d4508c5a2625ae73e4205f57536448c1c
wavelolz <rayhsu005@gmail.com>
2025-06-21 14:57:48 -0500

    testing: testing workflow date

--- Data Stream/action-test.py
+++ Data Stream/action-test.py
@@ -1,5 +1,2 @@
-print("this is hello from .py using github action")
-import os
-token = os.getenv("TOKEN")
-print(type(token))
-print(os.getenv("TOKEN"))
+from datetime import datetime, timedelta
+print(datetime.today())
